3c556da00c40c2e7b13a244c43c68c8cfca0f2a3
Ricardo Pereira <ricardoljp@gmail.com>
2020-08-17 17:54:37 -0300

    Fixed Routes.tsx, page now is loading

--- web/src/pages/routes.tsx
+++ web/src/pages/routes.tsx
@@ -8,9 +8,9 @@ import TeacherForm from '../pages/TeacherForm';
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact Component={ Landing } />
-            <Route path="/study" Component={ TeacherList } />
-            <Route path="/give-classes" Component={ TeacherForm } />
+            <Route path="/" exact component={ Landing } />
+            <Route path="/study" component={ TeacherList } />
+            <Route path="/give-classes" component={ TeacherForm } />
         </BrowserRouter>
     );
 }
